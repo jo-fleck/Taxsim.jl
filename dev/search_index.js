64e4e5b509406bf5c4e11e4695e96bd4b0fe5127
Documenter.jl <documenter@juliadocs.github.io>
2021-02-26 13:44:00 +0000

    build based on 9aa8a10

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Taxsim","category":"page"},{"location":"#Taxsim","page":"Home","title":"Taxsim","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Taxsim]","category":"page"}]
+}
